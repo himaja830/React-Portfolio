8e12885dc3cc56e52e94071112f01ea5485dcb69
Himaja830 <Himaja.Sriramaneni@gmail.com>
2020-08-24 09:15:41 -0500

    Added Resume

--- src/App.js
+++ src/App.js
@@ -47,8 +47,7 @@ class App extends React.Component {
               <Link className="nav-link" to="/">Home</Link>
               <Link className="nav-link" to="/about">About</Link>
               <Link className="nav-link" to="/contact">Contact</Link>
-              <Link className="nav-link" to="https://docs.google.com/document/d/1k3QMn_XeigwSdIjmDWHl84-_EBCMQ16u96NxXKUCMII/edit?usp=sharing">Resume</Link>
-              
+              <a href="https://docs.google.com/document/d/1k3QMn_XeigwSdIjmDWHl84-_EBCMQ16u96NxXKUCMII/edit?usp=sharing">Resume</a>
             </Nav>
 
           </Navbar.Collapse>
